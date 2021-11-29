2f13fe7402dc96eeeb754a8eceb9150546c74c51
maria-a02 <maria.aramaki@gmail.com>
2021-11-28 23:24:34 -0300

    Rails seeds for fair added

--- app/controllers/fairs_controller.rb
+++ app/controllers/fairs_controller.rb
@@ -1,5 +1,7 @@
 class FairsController < ApplicationController
   def index
+    @fairs = Fair.all
+    @fair = Fair.new
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Fair.destroy_all
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'feriasample.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+puts csv_text
+csv.each do |row|
+    f = Fair.new
+    f.name = row['NAME']
+    f.description = row['DESCRIPTION']
+    f.main_street = row['MAIN_STREET']
+    f.first_street = row['FIRST_STREET']
+    f.second_street = row['SECOND_STREET']
+    f.commune = row['COMMUNE']
+    f.day = row['DAY']
+    f.start_time = row['START_TIME']
+    f.end_time = row['END_TIME']
+    f.save
+end
