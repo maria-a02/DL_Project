42af2601e45dcda4e237492ce766e2fd50842b41
maria-a02 <maria.aramaki@gmail.com>
2021-11-28 22:25:30 -0300

    Opinion model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_005911) do
+ActiveRecord::Schema.define(version: 2021_11_29_012455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_11_29_005911) do
     t.string "day"
     t.time "start_time"
     t.time "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "opinions", force: :cascade do |t|
+    t.string "title"
+    t.string "comment"
+    t.string "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
