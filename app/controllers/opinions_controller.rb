e71bed298b69e7e984ef9480837aaf89051dedf1
maria-a02 <maria.aramaki@gmail.com>
2021-11-28 22:27:51 -0300

    Opinion model added

---
+++ app/controllers/opinions_controller.rb
@@ -0,0 +1,13 @@
+class OpinionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  resources :opinions
   resources :fairs
   devise_for :users
-  get 'fairs/index'
-  get 'fairs/show'
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
