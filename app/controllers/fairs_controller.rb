53c6fe7a1e5877b58a0e9b9cc4eecf5e0e41b8aa
maria-a02 <maria.aramaki@gmail.com>
2022-01-03 02:53:15 -0300

    Leaflet maps and background added

--- app/controllers/fairs_controller.rb
+++ app/controllers/fairs_controller.rb
@@ -1,4 +1,6 @@
 class FairsController < ApplicationController
+before_action :set_fair, only: %i[ show edit update destroy ]
+before_action :authenticate_user! 
   def index
     @fairs = Fair.all.order("commune ASC")
     @fair = Fair.new
@@ -7,10 +9,16 @@ class FairsController < ApplicationController
   def show
     @fair = Fair.find(params[:id])
     @opinions = @fair.opinions
+    @fairs = Fair.find(params[:id])
   end
 
   def filter
     @fairs = Fair.all
     @fairs = Fair.where(commune: params[:commune]) if params[:commune].present?
+  end
+
+  private
+  def set_fair
+    @fair = Fair.find(params[:id])
   end
 end
